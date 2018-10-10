ceb7a4dbb151c6cc16ae8ba03a6e432b10d6cb11
randilu <randilu.s@gmail.com>
2018-10-10 23:07:37 +0530

    adding methods to extract trending searches

--- trends.py
+++ trends.py
@@ -15,16 +15,16 @@ interest_over_time_df = pytrend.interest_over_time()
 print(interest_over_time_df)
 
 # Interest by Region
-interest_by_region_df = pytrend.interest_by_region()
-print(interest_by_region_df.head())
+# interest_by_region_df = pytrend.interest_by_region()
+# print(interest_by_region_df.head())
 #
 # # Related Queries, returns a dictionary of dataframes
 # related_queries_dict = pytrend.related_queries()
 # print(related_queries_dict)
 #
-# # Get Google Hot Trends data
-# trending_searches_df = pytrend.trending_searches()
-# print(trending_searches_df.head())
+# Get Google Hot Trends data
+trending_searches_df = pytrend.trending_searches()
+print(trending_searches_df.head())
 #
 # # Get Google Top Charts
 # top_charts_df = pytrend.top_charts(cid='actors', date=201611)
