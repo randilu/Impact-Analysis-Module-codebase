448342d6692ace1bf9a9080950884111faeb38ed
randilu <randilu.s@gmail.com>
2018-12-13 02:25:57 +0530

    normalizing trends

--- src/data/fetch_trend_data.py
+++ src/data/fetch_trend_data.py
@@ -2,41 +2,38 @@ from pandas import Series, DataFrame
 from pytrends.request import TrendReq
 
 import numpy as np
+import pandas as pd
 from sklearn import preprocessing
 
 pytrends = TrendReq(hl='en-US', tz=330)
 
-kw_list = ["Prime Minister"]
+kw_list = ["Storm", "mahinda", "Prime Minister", "ranil", "home"]
 
 # Login to Google. Only need to run this once, the rest of requests will use the same session.
 pytrend = TrendReq()
 
 # Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
-pytrend.build_payload(kw_list, cat=0, timeframe='2018-10-20 2018-11-20', geo='LK')
+pytrend.build_payload(kw_list, cat=0, timeframe='2017-01-01 2017-12-31', geo='LK')
 
 # Interest Over Time
 interest_over_time_df = pytrend.interest_over_time()
-# print(interest_over_time_df)
-
-# df_new = interest_over_time_df[['date', 'Prime minister']]
-# print(df_new)
-x_array = np.array(interest_over_time_df.get(kw_list[0]))
-
-
-normalized_X = preprocessing.normalize([x_array], 'max')
-for value in normalized_X:
-    y_array=value
-    # for element in value:
-    #     print(round(float(element), 2))
-interest_over_time_df['Normalized_Trend'] = Series(y_array, index=interest_over_time_df.index)
-json_data=interest_over_time_df.to_json('temp.json', orient='records', lines=True)
-
-print(y_array)
 print(interest_over_time_df)
 
-
+'''
+x_array = np.array(interest_over_time_df.get(kw_list[0]))
+normalized_X = preprocessing.normalize([x_array], 'max')
+for value in normalized_X:
+    y_array = value
+    for element in value:
+        print(round(float(element), 2))
+interest_over_time_df['Normalized_Trend'] = Series(y_array, index=interest_over_time_df.index)
+json_data=interest_over_time_df.to_json('temp.json', orient='records', lines=True)
+print(y_array)
+print(interest_over_time_df)
 trend_file1 = open("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/raw/trend_data/trend_file1.xlsx", "w")
 trend_file1.write(str(interest_over_time_df))
+
+'''
 
 # Interest by Region
 # interest_by_region_df = pytrend.interest_by_region()
@@ -55,8 +52,19 @@ trend_file1.write(str(interest_over_time_df))
 # suggestions_dict = pytrend.suggestions(keyword='pizza')
 # print(suggestions_dict)
 
-x = interest_over_time_df.get(kw_list) #returns a numpy array
+x = interest_over_time_df.get(kw_list)  # returns a numpy array
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 df = DataFrame(x_scaled)
-print(df)
+rounded = df.round(2)
+rounded.index = interest_over_time_df.index
+df = interest_over_time_df.drop(columns=['isPartial'])
+columns = pd.DataFrame(columns=df.columns)
+rounded.columns = [columns]
+print(rounded)
+trend_file3 = rounded.to_csv(
+    "/home/randilu/fyp_impact analysis module/impact_analysis_module/data/raw/trend_data/trend_file3.csv", sep='\t',
+    encoding='utf-8')
+trend_file2 = open(
+    "/home/randilu/fyp_impact analysis module/impact_analysis_module/data/raw/trend_data/trend_file2.csv", "w")
+trend_file2.write(str(rounded))
