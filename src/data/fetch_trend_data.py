58231298fb256066bb6c7168797e7a7f22e84bcc
randilu <randilu.s@gmail.com>
2018-12-30 00:23:48 +0530

    adding capability to format data set

--- src/data/fetch_trend_data.py
+++ src/data/fetch_trend_data.py
@@ -5,8 +5,8 @@ from src.data.fetch_trend_data_utils import normalize_trends, remove_weekends
 pytrends = TrendReq(hl='en-US', tz=330)
 
 # kw_list = [['Storm', 'mahinda', 'Prime Minister', 'ranil', 'home'], ['Toyota']]
-# kw_list = [['Tea'], ['mahinda'], ['Prime Minister'], ['ranil'], ['home'], ['Toyota']]
-kw_list = [['Storm']]
+kw_list = [['Tea'], ['gsp+'], ['floods']]
+# kw_list = [['Storm']]
 
 
 # Login to Google. Only need to run this once, the rest of requests will use the same session.
@@ -32,7 +32,7 @@ for i, sub_list in enumerate(kw_list, start=0):
 
 # concatenating all the data frames to single data frame
 combined_trend_data_df = pd.concat(joined_trend_dfs_list, axis=1, sort=False)
-print(combined_trend_data_df)
+combined_trend_data_df['max_value'] = combined_trend_data_df.max(axis=1)
 # Interest by Region
 # interest_by_region_df = pytrend.interest_by_region()
 # print(interest_by_region_df.head())
@@ -54,16 +54,18 @@ print(combined_trend_data_df)
 stock_data_df = pd.read_csv("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/interim/cse_data/CSE_market_indices_2017 - Sheet1.csv",sep=None, thousands=',')
 stock_data_df['date'] = pd.to_datetime(stock_data_df['date'], format="%Y/%m/%d")
 stock_data_df = stock_data_df.set_index('date')
-print(stock_data_df)
-stock_data_df['new_col'] = (stock_data_df['Services'].pct_change()*100).round(2)
-# stock_data_df = (stock_data_df.pct_change()*100).round(2)
+# stock_data_df['new_col'] = (stock_data_df['Services'].pct_change()*100).round(2)
+stock_data_pct_change_df = (stock_data_df.pct_change()*100).round(2)
 #
 # combining trend data and stock data
 #
-result_df = pd.concat([combined_trend_data_df, stock_data_df], axis=1, sort=False)
+result_df = pd.concat([combined_trend_data_df, stock_data_df, stock_data_pct_change_df], axis=1, sort=False)
 result_df.reset_index(inplace=True)
 result_df.set_index('date', drop=False, inplace=True)
 result_df = remove_weekends(result_df)
-print(stock_data_df)
-print(result_df)
-stock_trend_combined = result_df.to_csv("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/interim/trend_data/stock_trend_combined.csv",sep='\t', encoding='utf-8')
+stock_trend_combined = result_df.to_csv("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/interim/trend_data/stock_trend_combined.csv",sep='\t', encoding='utf-8', index=False)
+formated_df = pd.read_csv("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/interim/trend_data/stock_trend_combined.csv",sep='\t', encoding='utf-8')
+formated_df.columns = formated_df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('\'', '').str.replace(',', '')
+formated_df = formated_df.dropna()
+print(formated_df)
+formated_df.to_csv("/home/randilu/fyp_impact analysis module/impact_analysis_module/data/processed/trend_data/stock_trend_formated.csv",sep='\t', encoding='utf-8', index=False)
